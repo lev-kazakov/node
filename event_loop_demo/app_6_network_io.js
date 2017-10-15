6e82a9eecc7d92237f3d328e196f9bd8e8595abd
Lev Kazakov <lev.kazakov@klarna.com>
2017-10-15 11:25:26 +0300

    minor

--- event_loop_demo/app_6_network_io.js
+++ event_loop_demo/app_6_network_io.js
@@ -9,9 +9,9 @@ socket.on('error', function (err) {
 
 socket.on('data', (data) => {
     console.log(`data recieved on socket: "${data}"`)
-});
+})
 
 console.log('performing network I/O')
 socket.connect(8080, () => {
     console.log('socket connected. waiting for some data to arrive.')
-});
+})
